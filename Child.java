0ca964de06e48b0cf04c1ec58ff5d61135401780
Sumit Yadav <114563493+YuvaSky@users.noreply.github.com>
2022-09-29 09:59:31 -0700

    29-9-2022

---
+++ Child.java
@@ -0,0 +1,15 @@
+package Corejava;
+
+//inherits the parent class
+	class Child extends Parent
+	{
+		public void speak()
+		{
+			System.out.println("I am Sumit yadav");
+		}
+		public void dance(){
+			System.out.println("I Cannot dance");
+		}
+		}
+
+	
