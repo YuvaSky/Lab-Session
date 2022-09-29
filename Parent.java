24ba29e39080a49dadd203a795c92cd3e59cf5bf
Sumit Yadav <114563493+YuvaSky@users.noreply.github.com>
2022-09-29 09:36:25 -0700

    Add files via upload

---
+++ Parent.java
@@ -0,0 +1,20 @@
+/* QUEstion 1 Create a class parent with default constructor and void sing method.
+ print something inside the constructor and methods Create child class which will
+ will inherit the property of parent and has methods void speak and void dance. print something inside the two methods.
+ Create another class inheritance and object of class and call all the methods.also mention the type of inheritance on the top of the program
+ Name : Sumit yadav
+ Date : 29/09/20022
+ */
+
+package Corejava;
+//Creating a default Constructor
+class Parent {
+	 Parent(){
+		System.out.println("first program");
+		}
+		public void  sing() {
+		System.out.println("I cannot Sing");
+		}
+    }
+
+
