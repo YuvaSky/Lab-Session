1276b7ced0756ab81cbfec2d4b3acd231133d81f
Sumit Yadav <114563493+YuvaSky@users.noreply.github.com>
2022-09-29 10:18:19 -0700

    Update and rename inheritanceandobject.java to inheritanceobject.java

---
+++ inheritanceobject.java
@@ -0,0 +1,15 @@
+package Corejava;
+//calling the both class
+class inheritanceandobject {
+	//Call the methods m
+	public static void main(String[]args){
+	//Creating object of child class
+	Child sc= new Child();
+ // call the  sub- class main method
+     sc.sing();
+     sc.speak();
+     sc.dance();
+      }
+     }
+
+
