cbaefc8d7feb9b4c63bbc4e65dc5a34fc704ae3e
Sumit Yadav <114563493+YuvaSky@users.noreply.github.com>
2022-09-29 10:13:40 -0700

    Add files via upload

---
+++ lastestVersionofJava.java
@@ -0,0 +1,15 @@
+/*Question : find the current version of Java
+Name  : Sumit Yadav
+Date  : 29/9/2022
+*/
+
+package Corejava;
+//
+class lastestVersionofJava {
+	// calling the lastest version of java
+	public static void main(String[]args) {
+		System.out.println("java Version is:"+System.getProperty("java.version"));
+	}
+
+	}
+
