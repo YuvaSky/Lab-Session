6ea4bc46077c4d54292943b8a141cd2527e0bb0c
Sumit Yadav <114563493+YuvaSky@users.noreply.github.com>
2022-09-30 18:03:46 +0530

    Update Calculation.java

--- Calculation.java
+++ Calculation.java
@@ -1,3 +1,13 @@
+ /*Ques3:Create a program for calculation.read three values from the user for operation
+first and second values as operands.
+third value as operator.
+if user press1 => print addition of first and second
+if user press2 => print subtraction of first and second
+if user press3 => print multiplication of first and second
+if user press4 => print division of first and second
+Name : Sumit Yadav 
+Date : 29/9/2002
+*/
 package Corejava;
 
 import java.util.Scanner;
