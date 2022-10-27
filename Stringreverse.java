3313ebf09aa8d6e79da168390da784e9f9651037
Sumit Yadav <114563493+YuvaSky@users.noreply.github.com>
2022-10-27 17:27:00 +0530

    Add files via upload

---
+++ Stringreverse.java
@@ -0,0 +1,31 @@
+/*
+ Question : WAP to sort string Array in Descending order.
+ Name : Sumit Yadav
+ Date : 27/10/2022
+ */
+package Corejava;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Stringreverse {
+
+	public static void main(String[] args) {
+		//creating scanner object to take input from user
+		Scanner sc = new Scanner(System.in);
+		//taking input from user
+		System.out.println("enter the String");
+		String arr=sc.nextLine();
+		char a[]=arr.toCharArray();
+		// traversing the array to find reverse string
+		System.out.println("reverse of a string is");
+		int i=a.length;
+		for(int j=i;j>0;j--){
+			System.out.print(a[j-1]);
+		}
+		
+	}
+
+
+	}
+
