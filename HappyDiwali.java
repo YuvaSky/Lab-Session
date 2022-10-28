bc98446714f209f800ad42cc0b91c31121147861
Sumit Yadav <114563493+YuvaSky@users.noreply.github.com>
2022-10-28 11:40:12 +0530

    Add files via upload

---
+++ HappyDiwali.java
@@ -0,0 +1,28 @@
+/*
+ Question : WAP to print Happy Diwali using any Loop
+ Name : Sumit Yadav
+ Date : 27/10/2022
+ */
+package Corejava;
+
+import java.util.Scanner;
+
+public class HappyDiwali {
+
+	public static void main(String[] args) {
+		//creating scanner object to take input from user
+		Scanner sc = new Scanner(System.in);
+		//taking input from user
+		System.out.println("Enter the number");
+		int a =sc.nextInt();
+		int[] arr=new int[a];
+		//traversing the array
+		for(int i=0;i<a;i++)
+		System.out.println("Happy Diwali");
+		}
+		}
+
+
+	
+
+
